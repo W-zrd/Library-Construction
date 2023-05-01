ff63e95f20e2270f46ab8f35f17922d5aac6ae8a
flxnzz <itszyro.47@gmail.com>
2023-05-01 21:35:45 +0700

    TP Modul 10 selesai

---
+++ AljabarLibraries/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class PersamaanKuadrat
+    {
+        public double[] akarPersamaanKuadrat(double[] persamaan)
+        {
+            double num1 = persamaan[0];
+            double num2 = persamaan[1];
+            double num3 = persamaan[2];
+            double diskriminan = (num2 * num2) - (4 * num1 * num3);
+
+            // tidak memiliki akar real
+            if (diskriminan < 0)
+            {
+                return new double[] { };
+            }
+            // memiliki akar real
+            else if (diskriminan == 0)
+            {
+                double x = -num2 / (2 * num1);
+                return new double[] { x };
+            }
+            else
+            {
+                // Persamaan memiliki dua akar real
+                double x1 = (-num2 + Math.Sqrt(diskriminan)) / (2 * num1);
+                double x2 = (-num2 - Math.Sqrt(diskriminan)) / (2 * num1);
+                return new double[] { x1, x2 };
+            }
+        }
+
+        public double[] hasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double y1 = a * a;
+            double y2 = 2 * a * b;
+            double y3 = b * b;
+
+            return new double[] { y1, y2, y3 };
+        }
+    }
+}
---
+++ DriverProject/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using AljabarLibraries;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] pers1 = { 1, -3, -10 };
+        double[] pers2 = { 2, -4 };
+        PersamaanKuadrat persKuadrat = new PersamaanKuadrat();
+
+        double[] hasilAkar = persKuadrat.akarPersamaanKuadrat(pers1);
+        double[] hasilKuadrat = persKuadrat.hasilKuadrat(pers2);
+
+        Console.WriteLine("Hasil akar persamaan kuadrat:");
+        foreach (double akar in hasilAkar)
+        {
+            Console.Write(akar + " ");
+        }
+
+        Console.WriteLine("\nHasil kuadrat:");
+        foreach (double hasil in hasilKuadrat)
+        {
+            Console.Write(hasil + " ");
+        }
+
+        
+    }
+}
